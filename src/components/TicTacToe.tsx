03d45bb731ffcec523d1b3b184dd85b6a9995e1c
ccorbin <calcorbin@gmail.com>
2023-01-02 09:07:20 -0600

    Create tictactoe board

--- src/components/TicTacToe.tsx
+++ src/components/TicTacToe.tsx
@@ -1,27 +1,186 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from 'react';
+import './TicTacToe.css';
 
 export default function TicTacToe() {
-    const [board, setBoard] = useState(Array(9).fill(null));
-    const [isCPUNext, setIsCPUNext] = useState(false);
-    const calculateWinner = (squares: (string | null)[]) => {
-        const lines = [
-            [0, 1, 2],
-            [3, 4, 5],
-            [6, 7, 8],
-            [0, 3, 6],
-            [1, 4, 7],
-            [2, 5, 8],
-            [0, 4, 8],
-            [2, 4, 6],
-        ];
-        for (let i = 0; i < lines.length; i++) {
-            const [a, b, c] = lines[i];
-            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-                return squares[a];
-            }
+  const [board, setBoard] = useState([
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', ''],
+  ]);
+  const [isCPUNext, setIsCPUNext] = useState(false);
+  const [winner, setWinner] = useState('');
+
+  useEffect(() => {
+    if (winner) return;
+    if (isCPUNext) {
+      cPUPlay();
+    }
+  }, [isCPUNext]);
+
+  const players = {
+    CPU: {
+      SYM: 'O',
+      NAME: 'CPU',
+    },
+    HUMAN: {
+      SYM: 'X',
+      NAME: 'You',
+    },
+  };
+
+  function sleep(milliseconds: number) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+      currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
+  }
+
+  function cPUPlay() {
+    if (winner) return;
+    sleep(1000);
+
+    const cPUMove = getCPUTurn();
+
+    board[cPUMove.arrayIndex][cPUMove.index] = players?.CPU?.SYM;
+
+    setBoard((board) => [...board]);
+    checkWinner();
+    setIsCPUNext(false);
+  }
+
+  function getCPUTurn() {
+    const emptyIndexes: { arrayIndex: number; index: number }[] = [];
+    board.forEach((row: string[], arrayIndex: number) => {
+      row.forEach((cell: string, index: number) => {
+        if (cell === '') {
+          emptyIndexes.push({ arrayIndex, index });
         }
-        return null;
+      });
+    });
+    const randomIndex = Math.floor(Math.random() * emptyIndexes.length);
+    return emptyIndexes[randomIndex];
+  }
+
+  function playFn(arrayIndex: number, index: number) {
+    if (isCPUNext) return;
+    if (winner) return;
+    board[arrayIndex][index] = players?.HUMAN?.SYM;
+    setBoard((board) => [...board]);
+    checkWinner();
+    setIsCPUNext(true);
+  }
+
+  function checkWinner() {
+    // check same row
+    for (let index = 0; index < board.length; index++) {
+      const row = board[index];
+      if (row.every((cell) => cell === players?.CPU?.SYM)) {
+        setWinner(players?.CPU?.NAME);
+        return;
+      } else if (row.every((cell) => cell === players?.HUMAN?.SYM)) {
+        setWinner(players?.HUMAN?.NAME);
+        return;
+      }
     }
 
-    const winner = calculateWinner(board);
+    // check same column
+    for (let i = 0; i < 3; i++) {
+      const column = board.map((row) => row[i]);
+      if (column.every((cell) => cell === players?.CPU?.SYM)) {
+        setWinner(players?.CPU?.NAME);
+        return;
+      } else if (column.every((cell) => cell === players?.HUMAN?.SYM)) {
+        setWinner(players?.HUMAN?.NAME);
+        return;
+      }
+    }
+
+    // check same diagonal
+    const diagonal1 = [board[0][0], board[1][1], board[2][2]];
+    const diagonal2 = [board[0][2], board[1][1], board[2][0]];
+    if (diagonal1.every((cell) => cell === players?.CPU?.SYM)) {
+      setWinner(players?.CPU?.NAME);
+      return;
+    } else if (diagonal1.every((cell) => cell === players?.HUMAN?.SYM)) {
+      setWinner(players?.HUMAN?.NAME);
+      return;
+    } else if (diagonal2.every((cell) => cell === players?.CPU?.SYM)) {
+      setWinner(players?.CPU?.NAME);
+      return;
+    } else if (diagonal2.every((cell) => cell === players?.HUMAN?.SYM)) {
+      setWinner(players?.HUMAN?.NAME);
+      return;
+    } else if (board.flat().every((cell) => cell !== '')) {
+      setWinner('draw');
+      return;
+    } else {
+      setWinner('');
+      return;
+    }
+  }
+
+  function displayWinner() {
+    if (winner === 'draw') {
+      return "It's a draw!";
+    } else if (winner) {
+      return `${winner} won!`;
+    }
+  }
+
+  function displayTurn() {
+    if (isCPUNext) {
+      return "CPU's turn";
+    } else {
+      return 'Your turn';
+    }
+  }
+
+  function playAgainFn() {
+    setBoard([
+      ['', '', ''],
+      ['', '', ''],
+      ['', '', ''],
+    ]);
+    setWinner('');
+    setIsCPUNext(false);
+  }
+
+  return (
+    <div className="container">
+      <div className="col">
+        <span onClick={() => playFn(0, 0)} className="cell">
+          {board[0][0]}
+        </span>
+        <span onClick={() => playFn(0, 1)} className="cell">
+          {board[0][1]}
+        </span>
+        <span onClick={() => playFn(0, 2)} className="cell">
+          {board[0][2]}
+        </span>
+      </div>
+      <div className="col">
+        <span onClick={() => playFn(1, 0)} className="cell">
+          {board[1][0]}
+        </span>
+        <span onClick={() => playFn(1, 1)} className="cell">
+          {board[1][1]}
+        </span>
+        <span onClick={() => playFn(1, 2)} className="cell">
+          {board[1][2]}
+        </span>
+      </div>
+      <div className="col">
+        <span onClick={() => playFn(2, 0)} className="cell">
+          {board[2][0]}
+        </span>
+        <span onClick={() => playFn(2, 1)} className="cell">
+          {board[2][1]}
+        </span>
+        <span onClick={() => playFn(2, 2)} className="cell">
+          {board[2][2]}
+        </span>
+      </div>
+    </div>
+  );
 }
